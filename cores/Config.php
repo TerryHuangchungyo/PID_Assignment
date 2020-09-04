6f197ecf2585362d2c0a7498a82402ce81a9e9aa
terryhuangchungyo <terry_huang@mail.chungyo.net>
2020-09-04 18:01:29 +0800

    Add Database script, Path designed

---
+++ cores/Config.php
@@ -0,0 +1,14 @@
+<?php
+class DB
+{
+    const dbuser = "root";
+    const dbpass = "root";
+    const dbhost = "localhost";
+    const dbport = "8809";
+    const dbname = "PID";
+}
+
+class Web
+{
+    const root = "/PID_Assignment/";
+}
---
+++ cores/Database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private static $db = null;
+    private $dblink;
+
+    private function __construct() {
+        try {
+            $dbStr = "mysql:host=".DB::dbhost.";dbname=".DB::dbname.";dbport=".DB::dbport.";";
+            $this->dblink = new PDO( $dbStr, DB::dbuser, DB::dbpass);
+            $this->dblink->query("SET NAMES utf8");
+        } catch( PDOException $e ) {
+            print "<script> console.log(Error!: " . $e->getMessage() . ")<script/>";
+            $this->dblink = null;
+        }
+    }
+
+    public function __destruct() {
+        $this->dblink = null;
+    }
+
+    public static function getDatabase() {
+        if( self::$db == null ) {
+            $db = new Database();
+        }
+        return $db->dblink;
+    }
+}
